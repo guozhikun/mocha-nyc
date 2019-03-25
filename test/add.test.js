5db4ee97e6f1c6b2dc90e6f126877d2743661ba2
guozhikun <935571097@qq.com>
2019-03-25 15:21:51 +0800

    修改

--- test/add.test.js
+++ test/add.test.js
@@ -3,7 +3,6 @@ var add= require('../js/add.js');
 var chai = require('chai');
 
 var expect = chai.expect;
-
 describe('加法函数的测试...', function() {
   it('1 加 1 应该等于 2', function() {
     expect(add(1, 1)).to.be.equal(2);
