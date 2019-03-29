a2582f03fa2e9258e935839af3ed549ae47546c6
guozhikun <935571097@qq.com>
2019-03-29 14:04:21 +0800

    mm

--- test/cover.test.js
+++ test/cover.test.js
@@ -1,1 +1,13 @@
 var cover= require('../js/cover.js');
+
+var chai = require('chai');
+
+var expect = chai.expect;
+describe('覆盖函数的测试...', function() {
+  it('传入-1和1', function() {
+    expect(cover(-1, 1)).to.be.equal(0);
+  });
+  /*it('传入2和1', function() {
+    expect(cover(2, 1)).to.be.equal(2);
+  });*/
+})
