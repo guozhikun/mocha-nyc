2c368827fd3844c55e03b2930a7b0ef55c187355
guozhikun <935571097@qq.com>
2019-03-29 14:05:02 +0800

    mm

--- js/add.js
+++ js/add.js
@@ -2,4 +2,10 @@
 function add(x, y) {
     return x + y;
   }
+let hello = "hello c++";
+if(hello === "hello c++"){
+  console.log("hello c++");
+} else {
+  hello = "hello c++";
+}
 module.exports = add;
